2fff3d24406cff6031fd37a62c0e8a370df1a3b7
Jake <github@jakewendt.com>
2011-02-23 12:47:13 -0800

    Added more structure.

--- generators/simply_sessions/templates/simply_sessions.rake
+++ generators/simply_sessions/templates/simply_sessions.rake
@@ -1,4 +1,5 @@
-#	From `script/generate calnet_authenticated` ...
-if Gem.searcher.find('calnet_authenticated')
-require 'calnet_authenticated/test_tasks'
+#	From `script/generate simply_sessions` ...
+unless Gem.source_index.find_name('jakewendt-simply_sessions').empty?
+	gem 'jakewendt-simply_sessions'
+	require 'simply_sessions/test_tasks'
 end
---
+++ lib/simply_sessions.rb
@@ -0,0 +1,29 @@
+require 'active_support'
+require 'ruby_extension'
+require 'simply_helpful'
+require 'simply_authorized'
+require 'acts_as_list'
+#module SimplySessions
+##	predefine namespace
+#end
+
+#	This doesn't seem necessary
+%w{models controllers}.each do |dir|
+	path = File.expand_path(File.join(File.dirname(__FILE__), '../app', dir))
+	ActiveSupport::Dependencies.autoload_paths << path
+	ActiveSupport::Dependencies.autoload_once_paths << path
+end
+
+if defined?(Rails) && Rails.env == 'test' && Rails.class_variable_defined?("@@configuration")
+	require 'active_support/test_case'
+	require 'factory_girl'
+#	require 'simply_pages/factories'
+#	else
+#		running a rake task
+end
+
+ActionController::Routing::Routes.add_configuration_file(
+	File.expand_path(
+		File.join(
+			File.dirname(__FILE__), '../config/routes.rb')))
+
