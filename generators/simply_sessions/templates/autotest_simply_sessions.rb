4e36dd7d6db19b0a547a66964e7f784136507c0e
Jake <github@jakewendt.com>
2011-10-10 12:20:37 -0700

    Cleaned up gem testing tasks and autotest.

--- generators/simply_sessions/templates/autotest_simply_sessions.rb
+++ generators/simply_sessions/templates/autotest_simply_sessions.rb
@@ -1,5 +1,3 @@
 #	From `script/generate simply_sessions` ...
-unless Gem.source_index.find_name('ccls-simply_sessions').empty?
-	gem 'ccls-simply_sessions'
-	require 'simply_sessions/autotest'
-end
+gem 'ccls-simply_sessions'
+require 'simply_sessions/autotest'
